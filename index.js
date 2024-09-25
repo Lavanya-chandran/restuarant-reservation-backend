c4b4c79217f5384a404a7a9991b165d2b24547d8
Lavanya-chandran <rclavanya04@gmail.com>
2024-09-25 11:19:07 +0530

    complete

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const express = require("express");
 const people = require("./controller/people");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv=require("dotenv")
+const dotenv = require("dotenv")
 const app = express();
+
+
 dotenv.config();
-mongoose.set("strictQuery",true);
-mongoose.connect("mongodb+srv://adibafaiyaz16:12345@perdetails.r9gaoey.mongodb.net/login",{
-  useNewUrlparser:true,useUnifiedTopology:true
-},).then(()=>console.log("Connected to DB"))
-.catch((err)=>console.log("error"))
+
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO, {
+  useNewUrlparser: true, useUnifiedTopology: true
+},).then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("error"))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/people",people);
+app.use("/people", people);
 
-app.listen(4000,()=>{
-    console.log("Server started at 4000");
+app.listen(4000, () => {
+  console.log("Server started at 4000");
 })
